7118336d58faf5dc483150ed8b388dc9509aa1a6
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-02-07 11:52:49 +0530

    added:Edited Person Details

--- Address_Book_System/src/addressBookSystem/AddressBook.java
+++ Address_Book_System/src/addressBookSystem/AddressBook.java
@@ -28,4 +28,79 @@ public class AddressBook implements AddressBookInterface{
 		System.out.println("Array of Person : "+personData);
 
 	}
+ 	public void editPerson() {
+
+		System.out.println("Enter person First Name to Edit person details");
+		Scanner edit = new Scanner(System.in);
+		String firstName = edit.nextLine();
+		boolean exists = false;
+		
+		  for (ContactPerson person:personData) {
+			 
+				  if (firstName.equals(person.firstName)) {
+					  exists = true;
+					  while (exists == true) {
+					  System.out.println("Select an option to edit\n" 
+							     +"1] Phone Number\n"
+							     +"2] City\n"
+							     +"3] State\n"
+							     +"4] ZipCode\n"
+							     +"5] Email\n"
+							     +"6] quit");
+
+					  		Scanner option = new Scanner(System.in);
+					  		int num = option.nextInt();
+					  		Scanner input = new Scanner(System.in);
+					  				switch (num) {
+										case 1:
+					  						System.out.println("Enter New Phone Number");
+					  						String phone = input.nextLine();
+					  						person.setPhone(phone);
+					  						System.out.println(person.toString());
+											System.out.println("*****Phone number Edited Successfully*****");
+											       break;
+					  					case 2:
+					  						System.out.println("Enter New City Name");
+					  						String city = input.nextLine();
+					  						person.setCity(city);
+					  						System.out.println(person.toString());
+											System.out.println("*****City Name Edited Successfully*****");
+												break;
+					  					case 3:
+					  						System.out.println("Enter New State Name");
+					  						String state = input.nextLine();
+					  						person.setState(state);
+					  						System.out.println(person.toString());
+											System.out.println("*****State Name Edited Successfully*****");
+												break;
+					  					case 4:
+					  						System.out.println("Enter New Zip Code");
+					  						String zip = input.nextLine();
+					  						person.setZip(zip);
+					  						System.out.println(person.toString());
+											System.out.println("*****Zip Code Edited Successfully*****");
+					  							break;
+					  					case 5:
+					  						System.out.println("Enter New Email Address");
+					  						String email = input.nextLine();
+					  						person.setEmail(email);
+					  						System.out.println(person.toString());
+											System.out.println("*****Email  Edited Successfully*****");
+												break;
+					  					case 6:
+					  						exists = false;
+					  							break;
+					  					default :
+				  						System.out.println("Invalid input");
+					  							break;
+					  				}
+					  		  }
+				  		}
+
+				  else {
+					  			System.out.println("Record Not Found");
+
+				  	}
+		      }
+ 	  	}
 }
