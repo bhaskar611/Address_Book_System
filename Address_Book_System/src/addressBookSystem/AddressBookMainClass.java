55a6029e4a068958726a47d7ef2bea6d4b00320d
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-02-07 10:04:42 +0530

    added:Welcome message

---
+++ Address_Book_System/src/addressBookSystem/AddressBookMainClass.java
@@ -0,0 +1,7 @@
+package addressBookSystem;
+
+public class AddressBookMainClass {
+	public static void main(String[] args) {	
+	System.out.println("*******WELCOME TO ADDRESS BOOK PROGRAM*******");
+}
+}
